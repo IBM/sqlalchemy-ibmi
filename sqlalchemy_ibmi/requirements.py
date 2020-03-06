50475f321546c0b7a197bb9c3c338f45d1e9c58c
naveen-ram <naveenaram@gmail.com>
2020-03-06 13:00:46 -0600

    Allow microsecond precision tests
    IBM i supports microsecond precision timestamps.
    
    Fixes: #48

--- sqlalchemy_ibmi/requirements.py
+++ sqlalchemy_ibmi/requirements.py
@@ -29,20 +29,6 @@ class Requirements(SuiteRequirements):
         return exclusions.closed()
 
     @property
-    def datetime_microseconds(self):
-        """target dialect supports representation of Python
-        datetime.datetime() with microsecond objects."""
-
-        return exclusions.closed()
-
-    @property
-    def time_microseconds(self):
-        """target dialect supports representation of Python
-        datetime.time() with microsecond objects."""
-
-        return exclusions.closed()
-
-    @property
     def unbounded_varchar(self):
         """Target database must support VARCHAR with no length"""
 
