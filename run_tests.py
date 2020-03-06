63d255ee2f176521429a3ed2d2405b8758d5514c
naveen-ram <naveenaram@gmail.com>
2020-03-06 09:18:57 -0600

    Replace AS400 text
    This change will also fix the renamed dialect.
    
    Fixes: #23

--- run_tests.py
+++ run_tests.py
@@ -1,3 +1,3 @@
 from sqlalchemy.dialects import registry
 
-registry.register("ibmi", "sqlalchemy_ibmi.backend", "AS400Dialect_pyodbc")
+registry.register("ibmi", "sqlalchemy_ibmi.base", "IBMiDb2Dialect")
