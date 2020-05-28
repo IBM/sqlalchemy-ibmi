0db142e6f05967acb12a1381816392451056eb01
naveen-ram <naveenaram@gmail.com>
2020-05-28 10:42:58 -0500

    Bump version to 0.9.0

--- sqlalchemy_ibmi/__init__.py
+++ sqlalchemy_ibmi/__init__.py
@@ -19,7 +19,7 @@
 # | Contributors: Jaimy Azle, Mike Bayer                                     |
 # +--------------------------------------------------------------------------+
 
-__version__ = '0.1.0-dev'
+__version__ = '0.9.0-dev'
 
 from . import base
 
