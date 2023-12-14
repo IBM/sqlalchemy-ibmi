ae784a19817b75f9fa9126b57026920b15561479
Kevin Adler <kadler@us.ibm.com>
2023-12-14 09:58:31 -0600

    Export Db2 type classes from package

--- sqlalchemy_ibmi/__init__.py
+++ sqlalchemy_ibmi/__init__.py
@@ -22,3 +22,60 @@
 __version__ = "0.9.3-dev"
 
 from . import base  # noqa: F401
+
+from .base import BIGINT
+from .base import BINARY
+from .base import BLOB
+from .base import BOOLEAN
+from .base import CHAR
+from .base import CLOB
+from .base import DATE
+from .base import DECIMAL
+from .base import FLOAT
+from .base import INTEGER
+from .base import NCHAR
+from .base import NCLOB
+from .base import NVARCHAR
+from .base import NUMERIC
+from .base import REAL
+from .base import SMALLINT
+from .base import TIME
+from .base import TIMESTAMP
+from .base import VARCHAR
+from .base import VARBINARY
+from .base import DOUBLE
+from .base import GRAPHIC
+from .base import VARGRAPHIC
+from .base import DBCLOB
+from .base import XML
+
+dialect = base.IBMiDb2Dialect
+
+__all__ = (
+    "BIGINT",
+    "BINARY",
+    "BLOB",
+    "BOOLEAN",
+    "CHAR",
+    "CLOB",
+    "DATE",
+    "DECIMAL",
+    "FLOAT",
+    "INTEGER",
+    "NCHAR",
+    "NCLOB",
+    "NVARCHAR",
+    "NUMERIC",
+    "REAL",
+    "SMALLINT",
+    "TIME",
+    "TIMESTAMP",
+    "VARCHAR",
+    "VARBINARY",
+    "DOUBLE",
+    "GRAPHIC",
+    "VARGRAPHIC",
+    "DBCLOB",
+    "XML",
+    "dialect",
+)
