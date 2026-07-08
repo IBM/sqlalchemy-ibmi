882c1531a8f8ec70c82bc11beccc2fd781bfe494
Korinne Adler <kadler@us.ibm.com>
2026-07-08 15:13:53 -0500

    Bump version: 0.9.4-rc → 0.9.4-rc1

--- sqlalchemy_ibmi/__init__.py
+++ sqlalchemy_ibmi/__init__.py
@@ -19,6 +19,6 @@
 # | Contributors: Jaimy Azle, Mike Bayer                                     |
 # +--------------------------------------------------------------------------+
 
-__version__ = "0.9.4-rc"
+__version__ = "0.9.4-rc1"
 
 from . import base  # noqa: F401
