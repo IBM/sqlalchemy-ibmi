dcfdf0aafa3dcccc746634525fa88280f47fc84a
Korinne Adler <kadler@us.ibm.com>
2026-07-09 17:59:07 -0500

    Bump version: 0.9.4 → 0.9.5-dev

--- sqlalchemy_ibmi/__init__.py
+++ sqlalchemy_ibmi/__init__.py
@@ -19,6 +19,6 @@
 # | Contributors: Jaimy Azle, Mike Bayer                                     |
 # +--------------------------------------------------------------------------+
 
-__version__ = "0.9.4"
+__version__ = "0.9.5-dev"
 
 from . import base  # noqa: F401
